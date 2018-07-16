c9bdabc16375aacce7d8294162f5e90f500dc246
Anton Lantukh <lantukh@bk.ru>
2018-07-17 00:30:27 +0300

    Настройка проекта

---
+++ src/components/AppRouter/AppRouter.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+
+class AppRouter extends Component {
+  render() {
+    return (
+      <Particles>
+        <div>App</div>
+      </Particles>
+    );
+  }
+}
+
+export default AppRouter;
